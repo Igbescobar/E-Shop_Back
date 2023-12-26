dedcba2bae9c2b132d7a6e8c58ed11d83768423f
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-12-26 11:56:51 +0100

    Changes to productModel and created categoryModel

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,33 +1,33 @@
 const Mongoose = require("mongoose");
 const { Schema } = Mongoose;
 
-const ProductSchema = new Schema({
+const CategorySchema = new Schema({
+  _id: {
+    type: Schema.ObjectId,
+    auto: true,
+  },
   name: {
     type: String,
     trim: true,
   },
-  imageUrl: {
-    type: String,
+  image: {
+    data: Buffer,
+    contentType: String,
   },
   description: {
     type: String,
     trim: true,
   },
-  quantity: {
-    type: Number,
-  },
-  price: {
-    type: Number,
-  },
   isActive: {
     type: Boolean,
     default: true,
   },
-  brand: {
-    type: Schema.Types.ObjectId,
-    ref: "Brand",
-    default: null,
-  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
   updated: Date,
   created: {
     type: Date,
@@ -35,4 +35,4 @@ const ProductSchema = new Schema({
   },
 });
 
-module.exports = Mongoose.model("Product", ProductSchema);
+module.exports = Mongoose.model("Category", CategorySchema);
